42ace53b2f9c2c6e3525b5bc55fea79ad4bb2439
kimseongjung <plznw4me@naver.com>
2018-09-15 15:14:22 +0900

    Dataprovider wally, load images and groundtruth on ram

--- Dataprovider.py
+++ Dataprovider.py
@@ -10,29 +10,58 @@ class Dataprovider():
         self.img_paths = glob.glob(os.path.join(self.imgdir , '*.{}'.format(self.imgext)) )
         self.img_names = map(lambda path : os.path.split(path)[-1] , self.img_paths)
         print '# images : {}'.format(len(self.img_paths))
+    @classmethod
+    def image_normalize(self , img):
+        return img/255.
+
+    def read_images_on_RAM(self ):
+        raise NotImplementedError
+
+    def read_gtbboxes_onRAM(self , label_path):
+        raise NotImplementedError
 
     @classmethod
-    def images_normalize(self , imgs):
-        return imgs/255.
+    def next_batch(self , imgs , labs  , n_batch):
+        indices = npr.randint(0, len(labs))
+        if n_batch == 1:
+            batch_xs = imgs[indices : indices +1]
+            batch_ys = labs[indices]
+        else:
+            batch_xs = imgs[indices]
+            batch_ys = labs[indices]
+
+        return batch_xs , batch_ys
 
 
+class Wally(Dataprovider):
+    def __init__(self ,imgdir ,imgext):
+        Dataprovider.__init__(self , imgdir ,imgext)
     def read_images_on_RAM(self):
-        print "{} images  loaded on RAM and Normalization".format(len(self.img_paths))
-        imgs=[]
+        pass;
 
-        for i,path in enumerate(self.img_paths):
-            try:
-                progress(i,len(self.img_paths))
-                img=np.asarray(Image.open(path).convert('RGB'))
-                img = img/255.
-                imgs.append(img)
+    def read_gtbboxes_onRAM(self , label_path):
+        f=open(label_path , 'r')
+        lines = f.readlines()
+        lines = lines[1:]
+        elements=[]
+        for line in lines:
+            fname , h, w, label , x1,y1,x2,y2 = line.split(',')
+            x1,y1,x2,y2 = map(lambda ele : int(ele.strip()) , [x1,y1,x2,y2])
+            elements.append([x1,y1,x2,y2,label])
 
-            except:
-                continue;
-        imgs = np.asarray(imgs)
-        print
-        return imgs
+        elements = np.asarray(elements)
+        print '# GroundTruths {} '.format(np.shape(elements))
+        return elements
 
+
+        pass;
+class PocKia(Dataprovider):
+    def __init__(self,imgdir ,imgext):
+        Dataprovider.__init__(self, imgdir, imgext)
+    def read_images_on_RAM(self):
+        pass;
+    def read_gtbboxes_onRAM(self , label_path):
+        pass;
     def _read_gtbboxes(self,label_path):
         f = open(label_path, 'r')
         lines = f.readlines()
@@ -65,22 +94,9 @@ class Dataprovider():
         print 'Error list : {}'.format(error_labels)
         return np.asarray(ret_gtbboxes)
 
-    @classmethod
-    def next_batch(self , imgs , labs  , n_batch):
-        indices = npr.randint(0, len(labs))
-        if n_batch == 1:
-            batch_xs = imgs[indices : indices +1]
-            batch_ys = labs[indices]
-        else:
-            batch_xs = imgs[indices]
-            batch_ys = labs[indices]
-
-        return batch_xs , batch_ys
-
 if __name__ == '__main__':
-    dataprovider = Dataprovider(imgdir='/Users/seongjungkim/Desktop/test' , imgext='jpg')
-    gt_bboxes = dataprovider.read_gtbboxes_onRAM('/Users/seongjungkim/Desktop/test/labels.txt')
-
+    wally = Wally(imgdir='./WallyDataset/images' , imgext='jpg')
+    gt_bboxes = wally.read_gtbboxes_onRAM('./WallyDataset/annotations/annotations.csv')
     print np.shape(gt_bboxes)
 
 
